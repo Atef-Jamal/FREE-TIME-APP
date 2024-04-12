a74f829623614054c763ad619f10b178013647b8
atef gamal <atefgmal778@gmail.com>
2024-04-12 10:23:59 +0200

    add https://free-time-beta.vercel.app/ to cors

--- server/server.ts
+++ server/server.ts
@@ -22,10 +22,14 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://free-time-beta.vercel.app",
+  })
+);
 
 export const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://free-time-beta.vercel.app" },
 });
 
 app.use(express.json());
