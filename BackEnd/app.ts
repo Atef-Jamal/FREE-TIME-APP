f7cef984ab90ba987758effaab4a043efde2f8a5
atef gamal <atefgmal778@gmail.com>
2024-05-07 19:09:09 +0300

    commit

--- BackEnd/app.ts
+++ BackEnd/app.ts
@@ -103,38 +103,47 @@ io.on("connection", (socet) => {
   });
 });
 
-setInterval(async () => {
-  const users = await User.find().select("dailyReward");
+setInterval(() => {
+  const grantRewardsToAllUsers = async () => {
+    const users = await User.find().select("dailyReward");
 
-  users.forEach(async (user) => {
-    if (user.dailyReward.days.length === 7) {
-      if (
-        !user.dailyReward.days.some((item: any) => item.isCollected === false)
-      ) {
+    users.forEach(async (user) => {
+      if (user.dailyReward.days.length === 7) {
+        if (
+          !user.dailyReward.days.some((item: any) => item.isCollected === false)
+        ) {
+          user.dailyReward = {
+            week: user.dailyReward.week + 1,
+            days: [{ day: 1, isCollected: false, reward: 50 }],
+          };
+
+          await user.save();
+        }
+      } else {
+        const previosDay =
+          user.dailyReward.days[user.dailyReward.days.length - 1];
+        const newObj = {
+          day: previosDay.day + 1,
+          isCollected: false,
+          reward: previosDay.day * 50,
+        };
         user.dailyReward = {
-          week: user.dailyReward.week + 1,
-          days: [{ day: 1, isCollected: false, reward: 50 }],
+          ...user.dailyReward,
+          days: [...user.dailyReward.days, newObj],
         };
 
         await user.save();
       }
-    } else {
-      const previosDay =
-        user.dailyReward.days[user.dailyReward.days.length - 1];
-      const newObj = {
-        day: previosDay.day + 1,
-        isCollected: false,
-        reward: previosDay.day * 50,
-      };
-      user.dailyReward = {
-        ...user.dailyReward,
-        days: [...user.dailyReward.days, newObj],
-      };
-
-      await user.save();
-    }
-  });
+    });
+  };
+  grantRewardsToAllUsers();
 }, 24 * 60 * 60 * 1000);
+
+setInterval(() => {
+  http.get(process.env.SERVER_BASE_URL!, () => {
+    console.log("success");
+  });
+}, 14 * 60 * 1000);
 
 server.listen(process.env.PORT, () => {
   connecteToMongodb();
