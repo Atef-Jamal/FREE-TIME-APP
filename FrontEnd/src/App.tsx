9be37d39608a1407b46ba1b7787ac90e4f1718c0
atef gamal <atefgmal778@gmail.com>
2024-11-25 17:41:05 +0200

    commited

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,11 +4,11 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import io from "socket.io-client";
 import { setOnlineUsers, setSocet } from "./context/StateManeger";
 import { useListenToSocketEvents } from "./hooks";
-// import MobileChat from "./components/chats/publicChat/mobileChat/MobileChat";
-// import LoadingWebsite from "./components/others/LoadingWebsite";
-// import ProtectedPage from "./components/others/ProtectedPage";
-// import PageNotFound from "./components/errors/PageNotFound";
-// import AppError from "./components/errors/AppError";
+import MobileChat from "./components/chats/publicChat/mobileChat/MobileChat";
+import LoadingWebsite from "./components/others/LoadingWebsite";
+import ProtectedPage from "./components/others/ProtectedPage";
+import PageNotFound from "./components/errors/PageNotFound";
+import AppError from "./components/errors/AppError";
 import { useAppDispatch, useAppSelector } from "./context/Hooks";
 
 const Layout = lazy(() => import("./Pages/Layout"));
