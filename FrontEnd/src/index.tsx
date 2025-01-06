0925b47822de24f512f486e5f0fe3f19efdf315e
atef gamal <atefgmal778@gmail.com>
2025-01-06 12:24:20 +0200

    comit

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Provider } from "react-redux";
 import store from "./context/store";
 import App from "./App";
@@ -17,6 +18,7 @@ root.render(
     <Provider store={store}>
       {/* <QueryClientProvider client={queryClient}> */}
       <App />
+      <SpeedInsights />
       {/* </QueryClientProvider> */}
     </Provider>
   </StrictMode>,
