3e30a9b1060854415ec89c4e227277bef98fbcd8
Atef-Jamal <111036065+Atef-Jamal@users.noreply.github.com>
2026-07-13 05:32:08 +0300

    Update app.ts

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -25,29 +25,6 @@ app.use(passport.initialize());
 await connecteToMongodb();
 await connectToRedis();
 
-const g = async () => {
-  const numbers = Array.from({ length: 10000 }, (_, i) => i);
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash("111111", salt);
-
-  const date = new Date();
-  const newUsers = numbers.map((number) => {
-    date.setSeconds(date.getSeconds() - number);
-    return {
-      name: `Anonymous-${number + 1}`,
-      email: `anonymous-${number + 1}@gmail.com`,
-      password: hashedPassword,
-      profilePicture: "https://res.cloudinary.com/dql5bc50n/image/upload/v1780306060/avatar_uogqav.jpg",
-      createdAt: date,
-    };
-  });
-
-  await User.create(newUsers);
-  console.log("Done");
-};
-
-g();
 const server = http.createServer(app);
 
 export const { io, onlineUsers, activeConversations } = initializeSocket(server);
